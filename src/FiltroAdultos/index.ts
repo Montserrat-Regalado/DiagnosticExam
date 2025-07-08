175a8d1cbe00fa0f588e8010d15e7cfc1e279899
Montserrat Regalado <montserratregalado24@gmail.com>
2025-07-08 15:04:23 -0600

    Ejercicio filtro adultos mayores a 18

---
+++ src/FiltroAdultos/index.ts
@@ -0,0 +1,10 @@
+interface Usuario{
+    nombre:string;
+    edad:number;
+}
+function filtroAdultos(usuarios:Usuario[]){
+    const mayorEdad=usuarios.filter((u)=>u.edad>18)
+    return mayorEdad
+}
+const valores=filtroAdultos([{nombre:'Montse',edad:26},{nombre:'Leah',edad:6}])
+console.log(valores)
